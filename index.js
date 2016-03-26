fc6643dc5be1930a25413b9668e6a288ac9fdc14
Greg Gellene <greg.gellene@gmail.com>
2016-03-26 15:57:08 -0500

    Added template and data for portfolio page

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-/**
- * Created by greg on 2/28/16.
- */
+var jade = require('jade');
+var assign = require('object-assign');
+var fs = require('fs');
+var navigation = require('./data/links.json');
+var portfolio = require('./data/portfolio.json');
+var moment = require('moment');
+
+var index = fs.readFileSync('./templates/index.jade');
+var projects = fs.readFileSync('./templates/portfolio.jade');
+var resume = fs.readFileSync('./templates/resume.jade');
+
+var context = assign(navigation, portfolio, {moment:moment});
+
+function cbWrite(error, value){
+    if(error){
+        console.error(error);
+    }
+}
+
+fs.writeFile('./index.html', jade.render(index, assign(context, {filename: __dirname + '/templates/index.jade'})), undefined, cbWrite);
+fs.writeFile('./portfolio.html', jade.render(projects, assign(context, {filename: __dirname + '/templates/portfolio.jade'})), undefined, cbWrite);
+//fs.writeFile('./resume.html', jade.render(resume, context), undefined, cbWrite);
