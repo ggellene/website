5f660f0a141a3e9a4a8e2be4f11b75df9c4af5f7
Greg Gellene <greg.gellene@gmail.com>
2016-03-27 20:38:39 -0500

    Added resume page

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ var assign = require('object-assign');
 var fs = require('fs');
 var navigation = require('./data/links.json');
 var portfolio = require('./data/portfolio.json');
+var dResume = require('./data/resume.json');
 var moment = require('moment');
 
 var index = fs.readFileSync('./templates/index.jade');
 var projects = fs.readFileSync('./templates/portfolio.jade');
 var resume = fs.readFileSync('./templates/resume.jade');
 
-var context = assign(navigation, portfolio, {moment:moment});
+var context = assign(navigation, portfolio, dResume, {moment:moment});
 
 function cbWrite(error, value){
     if(error){
@@ -19,4 +20,4 @@ function cbWrite(error, value){
 
 fs.writeFile('./index.html', jade.render(index, assign(context, {filename: __dirname + '/templates/index.jade'})), undefined, cbWrite);
 fs.writeFile('./portfolio.html', jade.render(projects, assign(context, {filename: __dirname + '/templates/portfolio.jade'})), undefined, cbWrite);
-//fs.writeFile('./resume.html', jade.render(resume, context), undefined, cbWrite);
+fs.writeFile('./resume.html', jade.render(resume, assign(context, {filename: __dirname + '/templates/resume.jade'})), undefined, cbWrite);
