53676f724f0d48f845b93523c26ad7634d8cad22
Greg Gellene <greg.gellene@gmail.com>
2017-01-04 01:36:25 -0600

    updated resume

--- index.js
+++ index.js
@@ -36,21 +36,3 @@ fs.writeFile('./public/index.html', jade.render(index, assign(context, {filename
 fs.writeFile('./public/portfolio.html', jade.render(projects, assign(context, {filename: __dirname + '/templates/portfolio.jade'})), undefined, function(e,v){cbWrite(e,'portfolio')});
 fs.writeFile('./public/resume.html', jade.render(resume, assign(context, {filename: __dirname + '/templates/resume.jade'})), undefined, function(e,v){cbWrite(e,'resume')});
 fs.writeFile('./public/export/printResume.html', htmlResume, undefined, function(e,v){cbWrite(e,'export/resume')});
-
-//pdf.create(htmlResume, {
-//    format: 'letter',
-//    base: 'file://' + __dirname + '/public',
-//    viewportSize: {
-//        width: 1200,
-//        height: 1552
-//    },
-//    border: {
-//        "top": "1in",            // default is 0, units: mm, cm, in, px
-//        "right": ".5in",
-//        "bottom": ".5in",
-//        "left": ".5in"
-//    }
-//}).toFile('./public/export/resume.pdf', function(err, res) {
-//    if (err) return console.log('export/pdf\n' + err);
-//    console.log('export/pdf: success');
-//});
